ee0b65cd5af81d90c274c2b1e6ccea3a906eae27
michaelyg18 <michaelyg18@gmail.com>
2023-12-12 17:42:31 -0500

    Update epi_semi_sarsa.py
    changed neural network to predict whole action space

--- epi_semi_sarsa.py
+++ epi_semi_sarsa.py
@@ -8,40 +8,38 @@ import torch.nn as nn
 
 # neural network to approximate action-value
 class ActionValueApproximator(nn.Module):
-    def __init__(self, input_dim):
+    # input is state space, output is action space
+    def __init__(self, input_dim, output_dim):
         super().__init__()
         self.mlp = nn.Sequential(
-            nn.Linear(input_dim, input_dim*5),
-            nn.LeakyReLU(),
-            nn.Linear(input_dim*5, input_dim*2),
+            nn.Linear(input_dim, 256),
+            nn.ReLU(),
+            # nn.LayerNorm(256),
+            # nn.Linear(256, 64),
             # nn.ReLU(),
-            # nn.Linear((input_dim + 1) * 2, (input_dim + 1)),
-            nn.Tanh(),
-            nn.Linear(input_dim*2, 1)
+            nn.Linear(256, output_dim)
         )
 
     def forward(self, x):
+        x = torch.tensor(x).double()
         return self.mlp(x)
 
-# q_func is a neural network that returns a prediction
-def episodic_semigradient_nstep_sarsa(env, q_func, alpha, gamma, epsilon, n, num_episodes):
+def episodic_semigradient_nstep_sarsa(env, q_func, optimizer, gamma, epsilon, n, num_episodes):
     # initialize value-function weights arbitrarily
-    loss_func = nn.MSELoss()  # does reduction matter?
-    optimizer = torch.optim.Adam(q_func.parameters(), lr=alpha)
+    # all store and access operations (S, A, R) can take index mod n+1
+    loss_func = nn.MSELoss()
     return_hist = []
-    # all store and access operations (S, A, R) can take index mod n+1?
+    step_hist = []
 
     # loop for each episode
     for episode in range(1, num_episodes + 1):
         # initialize and store non-terminal S0
         state, _ = env.reset()
         states = [state]
-        # states = [0] * (n + 1)
 
         # select and store action A0 ~ pi(·|S0), or epsilon greedy wrt q(S0, ·, w)
-        action = epsilon_greedy(env, state, q_func, epsilon/np.sqrt(episode))
-        # action = epsilon_greedy(env, state, q_func, epsilon)
-        # action = softmax(env, state, q_func)
+        # action = epsilon_greedy(env, state, q_func, epsilon/np.sqrt(episode))
+        action = softmax(env, state, q_func)
         actions = [action]
         rewards = [0]
 
@@ -61,12 +59,9 @@ def episodic_semigradient_nstep_sarsa(env, q_func, alpha, gamma, epsilon, n, num
                     T_cap = t + 1
                 else:
                     # select and store At+1 ~ pi(·|St+1) or epsilon greedy wrt q(St+1, ·, w)
-                    next_action = epsilon_greedy(env, states[t], q_func, epsilon/np.sqrt(episode))
-                    # next_action = epsilon_greedy(env, states[t], q_func, epsilon)
-                    # next_action = softmax(env, next_state, q_func)
+                    # next_action = epsilon_greedy(env, states[t], q_func, epsilon/np.sqrt(episode))
+                    next_action = softmax(env, next_state, q_func)
                     actions.append(next_action)
-
-                # print("Step:", t, "State:", states[t], "action:", actions[t])
 
             # tau is time whose estimate is being updated
             tau = t - n + 1
@@ -75,25 +70,18 @@ def episodic_semigradient_nstep_sarsa(env, q_func, alpha, gamma, epsilon, n, num
                 sum_lim = min(tau + n, T_cap)
                 # summation formula: take slice of reward list, multiply in powers of gamma
                 G = np.sum(np.multiply(rewards[i:sum_lim + 1], np.power(gamma, np.arange(sum_lim - i + 1))))
-                G = torch.tensor([G])
+                G = torch.tensor(G).double()
 
                 if tau + n < T_cap:
-                    # here, pass state and action at time tau+n to q_func, get a prediction back (forward pass)
-                    state_action_tau_n = torch.tensor(np.append(states[tau+n], actions[tau+n]))
-                    q_pred = q_func(state_action_tau_n)
-                    # q_pred = q_func(state_action_tau_n).detach()
-                    # print(q_pred.item())
-                    # G += q_func(state_action_tau_n)
+                    # pass state and action at time tau+n to q_func, get a prediction back (forward pass)
+                    q_pred = q_func(states[tau+n])[actions[tau+n]].detach()
                     G += gamma**n * q_pred
 
-                # weight update
-                state_action_tau = torch.tensor(np.append(states[tau], actions[tau]))
-                q_pred = q_func(state_action_tau)
-                # MSE of St and Ut
-                # print(G)
+                # weight update step
+                # MSE of St and G, then backprop
+                q_pred = q_func(states[tau])[actions[tau]]
                 loss = loss_func(q_pred, G)
-                # optimizer.zero_grad()
-                # print(loss.item(), q_pred.item(), G.item())  # getting stuck?
+                optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
@@ -105,25 +93,18 @@ def episodic_semigradient_nstep_sarsa(env, q_func, alpha, gamma, epsilon, n, num
 
         total_return = np.sum(rewards)
         return_hist.append(total_return)
+        step_hist.append(t)
         print("Episode", episode, "Total return:", total_return)
-        # print("Model weights:", list(q_func.parameters()))
 
-    return return_hist
+    return return_hist, step_hist
 
 
 def epsilon_greedy(env, state, q_func, epsilon):
     # pick greedy with 1-epsilon chance
     if random.random() > epsilon:
-        # decide action with softmax loss?
         # for every possible action in current state, get q value and take argmax
-        q_vals = []
-        for action in range(env.action_space.n):
-            q_pred = q_func(torch.tensor(np.append(state, action)))
-            # q_pred = q_func(torch.tensor(np.append(state, action))).detach().numpy()
-            q_vals.append(q_pred)
-
-        return torch.argmax(torch.tensor(q_vals)).item()
-        # return np.argmax(q_vals)
+        q_vals = q_func(state).detach().numpy()
+        return np.argmax(q_vals)
 
     # explore with epsilon chance
     else:
@@ -131,42 +112,46 @@ def epsilon_greedy(env, state, q_func, epsilon):
         return env.action_space.sample()
 
 def softmax(env, state, q_func):
-    q_vals = []
-    actions = range(env.action_space.n)
-    for action in actions:
-        q_pred = q_func(torch.tensor(np.append(state, action))).detach().numpy()
-        q_vals.append(q_pred)
-
-    # softmax
+    # create tensor from states, turn it to double, detach it from network and convert to numpy
+    q_vals = q_func(state).detach().numpy()
+    # numerically stabilize by subtracting max action value from vector, then softmax
+    q_vals = q_vals - np.max(q_vals)
     softmax_probs = (np.exp(q_vals)/sum(np.exp(q_vals))).flatten()
-    return np.random.choice(actions, p=softmax_probs)
+
+    return np.random.choice(env.action_space.n, p=softmax_probs)
 
 
 if __name__ == '__main__':
+    # create MDP
+    # see episodes with human render mode, but runs a lot slower
 
-    # mountain car MDP
-    # see whats going on with human render mode, but runs a lot slower
-    # set max steps higher for mountaincar
     # env = gym.make('MountainCar-v0', render_mode=None)
     # env = gym.make('MountainCar-v0', render_mode="human")
-    # env._max_episode_steps = 1000
+    # env._max_episode_steps = 2500  # set max steps higher for mountaincar
+
     env = gym.make('LunarLander-v2', render_mode=None)
     # env = gym.make('LunarLander-v2', render_mode="human")
-    observation, info = env.reset(seed=42)  # set RNG generation seed for future episodes
+
+    # env = gym.make('CartPole-v1', render_mode=None)
+    # env = gym.make('CartPole-v1', render_mode="human")
+    observation, info = env.reset(seed=5)  # set RNG generation seed for future episodes
 
     # initialization
-    input_dim = observation.shape[0] + 1  # input dimension to approximator network is state space + the action taken
+    input_dim = observation.shape[0]  # input dimension to approximator network is state space
+    output_dim = env.action_space.n  # output dimension is action space
+
     # maybe add input dim of state * action to relate them more?
-    q_func = ActionValueApproximator(input_dim)
-    q_func.double()
-    # q_func.int()
-    alpha = 1e-3
-    gamma = .9  # can't be 1 for lunarlander
+    q_func = ActionValueApproximator(input_dim, output_dim).double()
+    # q_func.double()
+    alpha = 1e-2
+    gamma = 0.9
     epsilon = 1
     n = 4
-    num_episodes = 500
+    num_episodes = 10
 
-    return_hist = episodic_semigradient_nstep_sarsa(env, q_func, alpha, gamma, epsilon, n, num_episodes)
+    # initialize optimizer and run algorithm
+    optimizer = torch.optim.Adam(q_func.parameters(), lr=alpha)
+    return_hist = episodic_semigradient_nstep_sarsa(env, q_func, optimizer, gamma, epsilon, n, num_episodes)
     env.close()
 
     plt.figure()
