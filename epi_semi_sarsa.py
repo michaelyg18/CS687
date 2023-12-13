706425f50aa3486d1130dbebfc3b0723e4624c3b
michaelyg18 <michaelyg18@gmail.com>
2023-12-12 22:48:16 -0500

    final
    final

--- epi_semi_sarsa.py
+++ epi_semi_sarsa.py
@@ -24,15 +24,19 @@ class ActionValueApproximator(nn.Module):
         x = torch.tensor(x).double()
         return self.mlp(x)
 
+
 def episodic_semigradient_nstep_sarsa(env, q_func, optimizer, gamma, epsilon, n, num_episodes):
     # initialize value-function weights arbitrarily
     # all store and access operations (S, A, R) can take index mod n+1
     loss_func = nn.MSELoss()
     return_hist = []
-    step_hist = []
+    total_steps = 0
+    total_step_hist = []
+    loss_hist = []
 
     # loop for each episode
     for episode in range(1, num_episodes + 1):
+        total_loss = 0
         # initialize and store non-terminal S0
         state, _ = env.reset()
         states = [state]
@@ -74,8 +78,8 @@ def episodic_semigradient_nstep_sarsa(env, q_func, optimizer, gamma, epsilon, n,
 
                 if tau + n < T_cap:
                     # pass state and action at time tau+n to q_func, get a prediction back (forward pass)
-                    q_pred = q_func(states[tau+n])[actions[tau+n]].detach()
-                    G += gamma**n * q_pred
+                    q_pred = q_func(states[tau + n])[actions[tau + n]].detach()
+                    G += gamma ** n * q_pred
 
                 # weight update step
                 # MSE of St and G, then backprop
@@ -85,6 +89,8 @@ def episodic_semigradient_nstep_sarsa(env, q_func, optimizer, gamma, epsilon, n,
                 loss.backward()
                 optimizer.step()
 
+                total_loss += loss.item()
+
             if tau == T_cap - 1:
                 break
 
@@ -93,10 +99,13 @@ def episodic_semigradient_nstep_sarsa(env, q_func, optimizer, gamma, epsilon, n,
 
         total_return = np.sum(rewards)
         return_hist.append(total_return)
-        step_hist.append(t)
-        print("Episode", episode, "Total return:", total_return)
+        total_steps += t
+        total_step_hist.append(total_steps)
+        loss_hist.append(total_loss)
+        if episode % 100 == 0:
+            print("Episode", episode, "Total return:", total_return)
 
-    return return_hist, step_hist
+    return return_hist, total_step_hist, loss_hist
 
 
 def epsilon_greedy(env, state, q_func, epsilon):
@@ -111,14 +120,90 @@ def epsilon_greedy(env, state, q_func, epsilon):
         # random action in action space (n choices in discrete case)
         return env.action_space.sample()
 
+
 def softmax(env, state, q_func):
     # create tensor from states, turn it to double, detach it from network and convert to numpy
     q_vals = q_func(state).detach().numpy()
     # numerically stabilize by subtracting max action value from vector, then softmax
     q_vals = q_vals - np.max(q_vals)
-    softmax_probs = (np.exp(q_vals)/sum(np.exp(q_vals))).flatten()
+    softmax_probs = (np.exp(q_vals) / sum(np.exp(q_vals))).flatten()
 
     return np.random.choice(env.action_space.n, p=softmax_probs)
+
+
+def run_stats(env, alpha, gamma, epsilon, n, num_episodes, num_trials, env_name):
+    observation, info = env.reset(seed=5)  # set RNG generation seed for future episodes
+    # initialization
+    input_dim = observation.shape[0]  # input dimension to approximator network is state space
+    output_dim = env.action_space.n  # output dimension is action space
+
+    # maybe add input dim of state * action to relate them more?
+    q_func = ActionValueApproximator(input_dim, output_dim).double()
+
+    trial_hist = []
+    trial_loss_hist = []
+
+    # initialize optimizer and run algorithm
+    optimizer = torch.optim.Adam(q_func.parameters(), lr=alpha)
+
+    for i in range(num_trials):
+        return_hist, step_hist, loss_hist = episodic_semigradient_nstep_sarsa(env, q_func, optimizer, gamma, epsilon, n,
+                                                                              num_episodes)
+        trial_hist.append(return_hist)
+        trial_loss_hist.append(loss_hist)
+        print(f"Run {i+1} complete.")
+
+    # single run
+    ax1 = plt.figure().gca()
+    ax1.yaxis.get_major_locator().set_params(integer=True)
+    ax1.xaxis.get_major_locator().set_params(integer=True)
+    plt.plot(trial_hist[-1])
+    plt.title(f"esn-SARSA return over one run ({env_name})")
+    plt.xlabel("Episodes")
+    plt.ylabel("Total return")
+    plt.savefig(f'{env_name} single run.png')
+    # plt.show()
+
+    # average return over multiple runs
+    trial_hist = np.array(trial_hist)
+    mean_hist = np.mean(trial_hist, axis=0)
+    std_hist = np.std(trial_hist, axis=0)
+    ax1 = plt.figure().gca()
+    ax1.yaxis.get_major_locator().set_params(integer=True)
+    ax1.xaxis.get_major_locator().set_params(integer=True)
+    plt.plot(range(1, num_episodes + 1), mean_hist, color='b')
+    plt.fill_between(range(1, num_episodes + 1), mean_hist - std_hist, mean_hist + std_hist, color='c')
+    plt.title(f"Average return of esn-SARSA over {num_trials} runs ({env_name})")
+    plt.xlabel("No. episodes")
+    plt.ylabel("Average return")
+    plt.savefig(f'{env_name} avg return.png')
+    # plt.show()
+
+    # average loss over multiple runs
+    trial_loss_hist = np.array(trial_loss_hist)
+    mean_hist = np.mean(trial_loss_hist, axis=0)
+    std_hist = np.std(trial_loss_hist, axis=0)
+    ax1 = plt.figure().gca()
+    ax1.yaxis.get_major_locator().set_params(integer=True)
+    ax1.xaxis.get_major_locator().set_params(integer=True)
+    plt.plot(range(1, num_episodes + 1), mean_hist, color='b')
+    plt.fill_between(range(1, num_episodes + 1), mean_hist - std_hist, mean_hist + std_hist, color='c')
+    plt.title(f"Average Loss of esn-SARSA over {num_trials} runs ({env_name})")
+    plt.xlabel("No. episodes")
+    plt.ylabel("Average Loss")
+    plt.savefig(f"{env_name} avg loss.png")
+    # plt.show()
+
+    # steps vs episodes
+    ax1 = plt.figure().gca()
+    ax1.yaxis.get_major_locator().set_params(integer=True)
+    ax1.xaxis.get_major_locator().set_params(integer=True)
+    plt.plot(step_hist, range(1, num_episodes + 1))
+    plt.title(f"esn-SARSA steps vs episodes ({env_name})")
+    plt.xlabel("Steps")
+    plt.ylabel("Episodes")
+    plt.savefig(f'{env_name} steps.png')
+    # plt.show()
 
 
 if __name__ == '__main__':
@@ -128,36 +213,14 @@ if __name__ == '__main__':
     # env = gym.make('MountainCar-v0', render_mode=None)
     # env = gym.make('MountainCar-v0', render_mode="human")
     # env._max_episode_steps = 2500  # set max steps higher for mountaincar
-
     env = gym.make('LunarLander-v2', render_mode=None)
     # env = gym.make('LunarLander-v2', render_mode="human")
-
-    # env = gym.make('CartPole-v1', render_mode=None)
-    # env = gym.make('CartPole-v1', render_mode="human")
-    observation, info = env.reset(seed=5)  # set RNG generation seed for future episodes
-
-    # initialization
-    input_dim = observation.shape[0]  # input dimension to approximator network is state space
-    output_dim = env.action_space.n  # output dimension is action space
-
-    # maybe add input dim of state * action to relate them more?
-    q_func = ActionValueApproximator(input_dim, output_dim).double()
-    # q_func.double()
-    alpha = 1e-2
-    gamma = 0.9
-    epsilon = 1
-    n = 4
-    num_episodes = 10
-
-    # initialize optimizer and run algorithm
-    optimizer = torch.optim.Adam(q_func.parameters(), lr=alpha)
-    return_hist = episodic_semigradient_nstep_sarsa(env, q_func, optimizer, gamma, epsilon, n, num_episodes)
+    run_stats(env, alpha=5e-4, gamma=0.9, epsilon=1, n=4, num_episodes=500, num_trials=5, env_name="Lunar Lander")
     env.close()
 
-    plt.figure()
-    plt.plot(return_hist)
-    plt.title("Total return over episodes")
-    plt.xlabel("Episodes")
-    plt.ylabel("Total return")
-    plt.show()
+    env = gym.make('CartPole-v1', render_mode=None)
+    # env = gym.make('CartPole-v1', render_mode="human")
+    run_stats(env, alpha=1e-4, gamma=0.9, epsilon=1, n=4, num_episodes=10000, num_trials=5, env_name="Cart Pole")
+    env.close()
+
 
